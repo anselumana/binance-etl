c360e92fedd41e7805da845bff54c283ff72ee9c
anselumana <anselumana@gmail.com>
2024-09-23 14:47:36 +0200

    optionally specify config file with CONFIG_PATH  env var

--- binance_etl/library/utils.py
+++ binance_etl/library/utils.py
@@ -1,14 +1,10 @@
+import os
 import json
 
 
-def flatten(list_of_lists: list):
-    return sum(list_of_lists, [])
-
-def is_none_or_empty(s: str) -> bool:
-    return s is None or s.strip() == ''
-
-def load_config(path: str = None) -> dict:
-    with open(path or './config.json') as config:
+def load_config() -> dict:
+    path = os.getenv('CONFIG_PATH', 'config.json')
+    with open(path) as config:
         return json.loads(config.read())
 
 def logger_name_with_symbol(name: str, symbol: str):
